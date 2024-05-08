3fb4612d4e592bc497771e046138a9344617daa3
Małgorzata Kwiecień <kwiecien.malgorzata.00@wp.pl>
2024-05-08 23:32:12 +0200

    initial commit

---
+++ aora/app/(tabs)/_layout.tsx
@@ -0,0 +1,75 @@
+import { View, Text, Image, ImageSourcePropType, ColorValue } from 'react-native'
+import { Tabs, Redirect } from 'expo-router'
+import { icons } from '../../constants'
+
+interface TabIconProps {
+	icon: ImageSourcePropType
+	color: ColorValue
+	name: string
+	focused: boolean
+}
+
+const TabIcon = ({ icon, color, name, focused }: TabIconProps) => {
+	return (
+		<View className='flex items-center justify-center gap-2'>
+			<Image source={icon} resizeMode='contain' tintColor={color} className='w-6 h-6' />
+			<Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs `} style={{ color: color }}>
+				{name}
+			</Text>
+		</View>
+	)
+}
+
+const TabsLayout = () => {
+	return (
+		<>
+			<Tabs
+				screenOptions={{
+					tabBarActiveTintColor: '#FFA001',
+					tabBarInactiveTintColor: '#CDCDE0',
+					tabBarShowLabel: false,
+					tabBarStyle: {
+						backgroundColor: '#161622',
+						borderTopWidth: 1,
+						borderTopColor: '#232533',
+						height: 84,
+					},
+				}}>
+				<Tabs.Screen
+					name='home'
+					options={{
+						title: 'Home',
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => <TabIcon icon={icons.home} color={color} name='Home' focused={focused} />,
+					}}
+				/>
+				<Tabs.Screen
+					name='bookmark'
+					options={{
+						title: 'Bookmark',
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => <TabIcon icon={icons.bookmark} color={color} name='Bookmark' focused={focused} />,
+					}}
+				/>
+				<Tabs.Screen
+					name='create'
+					options={{
+						title: 'Create',
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => <TabIcon icon={icons.plus} color={color} name='Create' focused={focused} />,
+					}}
+				/>
+				<Tabs.Screen
+					name='profile'
+					options={{
+						title: 'Profile',
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => <TabIcon icon={icons.profile} color={color} name='Profile' focused={focused} />,
+					}}
+				/>
+			</Tabs>
+		</>
+	)
+}
+
+export default TabsLayout
---
+++ aora/components/CustomButton.tsx
@@ -0,0 +1,21 @@
+import { TouchableOpacity, Text, GestureResponderEvent } from 'react-native'
+
+interface CustomButtonProps {
+	title: string
+	handlePress: (event: GestureResponderEvent) => void
+	containerStyles: string
+	textStyles?: string
+	isLoading?: boolean
+}
+
+export const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoading }: CustomButtonProps) => {
+	return (
+		<TouchableOpacity
+			onPress={handlePress}
+			activeOpacity={0.7}
+			className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
+			disabled={isLoading}>
+			<Text className={`text-primary font-psemibold text-lg ${textStyles}`}>{title}</Text>
+		</TouchableOpacity>
+	)
+}
